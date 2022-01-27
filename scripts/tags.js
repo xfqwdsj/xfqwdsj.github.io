47d68af3e0a6d0511ae5c868043a6974674c53ba
HMLTFan <xfqwdsj@qq.com>
2022-01-28 00:12:09 +0800

    l

--- scripts/tags.js
+++ scripts/tags.js
@@ -1,12 +1,14 @@
 hexo.extend.tag.register('message', function(args, content){
-    var icon = args.splice(0, 1);
+    var iconAndTitle = args.splice(0, 1).split(',');
+    var icon = iconAndTitle[0];
+    var title = iconAndTitle[1];
     var classes = args;
-    if (icon != null) iconEl = `<i class="fas fa-${icon} mr-2"></i>`
+    if (icon != null) icon = `<i class="fas fa-${icon} mr-2"></i>`
+    if (title != null && title != "") title = `${title}\n`
     return `
     <article class="message message-immersive ${classes.join(' ')}">
         <div class="message-body">
-            ${iconEl}
-            ${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}
+            ${icon}${title}${hexo.render.renderSync({text: content, engine: 'markdown'})}
         </div>
     </article>
     `;
