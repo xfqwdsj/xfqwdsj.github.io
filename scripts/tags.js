17cbff766fcfab9c93edd8f6c7d08b340d305b38
HMLTFan <xfqwdsj@qq.com>
2022-01-27 23:29:34 +0800

    bug

--- scripts/tags.js
+++ scripts/tags.js
@@ -3,7 +3,7 @@ hexo.extend.tag.register('message', function(args, content){
     var classes = args;
     if (icon != null) iconEl = `<i class="fas fa-${icon} mr-2"></i>`
     return `
-    <article class="message message-immersive ${type.join(' ')}">
+    <article class="message message-immersive ${classes.join(' ')}">
         <div class="message-body">
             ${iconEl}
             ${hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('')}
