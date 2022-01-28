9414fea049acf5e1d4f71a800308d279aa622a30
HMLTFan <xfqwdsj@qq.com>
2022-01-28 22:44:38 +0800

    f

--- scripts/tags.js
+++ scripts/tags.js
@@ -10,8 +10,9 @@ hexo.extend.tag.register('icon', function(args) {
  * @param {string} color The color of this message. Usable: dark, primary, link, info, success, warning, danger.
  * @param {string} icon The icon of this message, can not be set.
  * @param {string} title The header of this message, can not be set, supported Markdown.
+ * @param {string} styles Additional styles of this message, can not be set. Usable: [small, medium, large].
  * @example
- * {% message color:danger icon:info-circle 'title:Very danger!' %}
+ * {% message color:danger icon:info-circle 'title:Very danger!' 'style:small' %}
  *     **You are in danger.**
  * {% endmessage %}
  */
@@ -19,6 +20,7 @@ hexo.extend.tag.register('message', function(args, content) {
     var color = 'dark';
     var icon = '';
     var title = '';
+    var styles = '';
     var header = '';
     args.forEach(element => {
         var key = element.split(':')[0];
@@ -34,6 +36,16 @@ hexo.extend.tag.register('message', function(args, content) {
                 case 'title':
                     title = value;
                     break;
+                case 'style':
+                    var stylesArray = value.split(' ');
+                    var processed = [];
+                    stylesArray.forEach(styleElement, index => {
+                        if (styleElement != null && styleElement != undefined && styleElement != '') {
+                            processed.push(`is-${styleElement}`);
+                        }
+                    });
+                    styles = processed.join(' ');
+                    break;
             }
         }
     });
@@ -45,7 +57,7 @@ hexo.extend.tag.register('message', function(args, content) {
         `
     }
     return `
-    <article class="message is-${color}">
+    <article class="message is-${color} ${styles}">
         ${header}
         <div class="message-body">
         ${hexo.render.renderSync({text: content, engine: 'md'})}
